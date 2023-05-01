6ce2e95f506aebe353eb9ce9648d7a7e0861e568
Yuliya <yulya.al.belous@gmail.com>
2023-05-01 09:24:57 +0300

    Update

--- src/components/PageWithBootstrap.js
+++ src/components/PageWithBootstrap.js
@@ -2,7 +2,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Table from 'react-bootstrap/Table';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, Container, Form, NavDropdown} from "react-bootstrap";
+import {Button, Container} from "react-bootstrap";
 import {Plus, PencilFill, CardList, FiletypePdf, PersonCircle, PersonFillLock, HouseFill, Trash3Fill, FileTextFill} from "react-bootstrap-icons";
 
 
@@ -10,44 +10,6 @@ const PageWithBootstrap = () => {
 
 return(
     <>
-        {/*<Navbar bg="light" expand="lg">
-            <Container fluid>
-                <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
-                        navbarScroll
-                    >
-                        <Nav.Link href="#action1">Home</Nav.Link>
-                        <Nav.Link href="#action2">Link</Nav.Link>
-                        <NavDropdown title="Link" id="navbarScrollingDropdown">
-                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action4">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action5">
-                                Something else here
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link href="#" disabled>
-                            Link
-                        </Nav.Link>
-                    </Nav>
-                    <Form className="d-flex">
-                        <Form.Control
-                            type="search"
-                            placeholder="Search"
-                            className="me-2"
-                            aria-label="Search"
-                        />
-                        <Button variant="outline-success">Search</Button>
-                    </Form>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>*/}
         <Navbar bg="primary" variant="dark">
             <Container fluid>
                 <Navbar.Brand className="ms-4" href="#home"> <FileTextFill size={30}/> Invoice</Navbar.Brand>
